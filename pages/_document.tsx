3e04a39524e0e9bdcf01af0cf4d654df21daea42
vaguebrownfox01 <jeevankylash@outlook.com>
2023-09-05 16:10:57 +0530

    updates

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,35 +1,38 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="description"
-            content="happy teacher's day 2023"
-          />
-          {/* <meta property="og:site_name" content="nextjsconf-pics.vercel.app" /> */}
-          <meta
-            property="og:description"
-            content="See pictures from Next.js Conf and the After Party."
-          />
-          {/* <meta property="og:title" content="Next.js Conf 2022 Pictures" />
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<link rel="icon" href="/spr-logo.png" />
+					<meta
+						name="description"
+						content="happy teacher's day 2023"
+					/>
+					<meta
+						property="og:site_name"
+						content="spirelab-happy-teachers-day.vercel.app"
+					/>
+					<meta
+						property="og:description"
+						content="Notes from SPIRE Lab family"
+					/>
+					{/* <meta property="og:title" content="Next.js Conf 2022 Pictures" />
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:title" content="Next.js Conf 2022 Pictures" /> */}
-          <meta
+					{/* <meta
             name="twitter:description"
             content="See pictures from Next.js Conf and the After Party."
-          />
-        </Head>
-        <body className="bg-black antialiased">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+          /> */}
+				</Head>
+				<body className="bg-black antialiased">
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
-export default MyDocument
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -50,18 +50,25 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
 					/>
 				)}
 				<div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
-					<div className="after:content relative mb-5 flex h-[256px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
+					<div className="after:content relative mb-4 flex h-[300px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
 						<div className="absolute inset-0 flex items-center justify-center opacity-20">
 							<span className="flex max-h-full max-w-full items-center justify-center">
 								<Bridge />
 							</span>
 							<span className="absolute bottom-0 left-0 right-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
 						</div>
+						<Image
+							alt="spire logo"
+							className="transform rounded-lg brightness-90 transition will-change-auto group-hover:brightness-110"
+							src={`/spr-logo.png`}
+							width={72}
+							height={72}
+						/>
 						<h1 className="mb-2 mt-4 text-base font-bold uppercase tracking-widest">
 							HAPPY TEACHERS DAY 2023
 						</h1>
 						<p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-							*Insert brief text here*
+							Notes from SPIRE Lab Family!
 						</p>
 					</div>
 					{/* <>
@@ -95,9 +102,9 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
 						{notes2.map((info) => (
 							<div
 								key={info.Timestamp}
-								className="after:content group relative mb-5 block w-full  justify-end p-8 after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
+								className="after:content group relative mb-4 block w-full  justify-end p-8 after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
 							>
-								<p className="mb-2 mt-4 whitespace-pre-line text-center text-base font-bold tracking-wide text-white">
+								<p className="mb-2 mt-4 whitespace-pre-line text-center text-base font-normal tracking-wide text-white">
 									{info["Your note"]}
 								</p>
 								<h1 className="mr-4 text-right text-white/75">
@@ -238,5 +245,19 @@ const notes2 = [
 		"Display your name under the message?": "Yes",
 		"Your note":
 			"শব্দের একটি সিম্ফনি\nমন বিস্ময়ে ভরে যায়\nএকটি নতুন গানের জন্ম হয়\n\n- পৃথিবী সবার শেখার জন্য একটি বড় শ্রেণীকক্ষ।\n(The world is a big-classroom for everyone to be inspired)\n\nThank you, Prasanta Sir for InSPIREing us",
+	},
+	{
+		Timestamp: "9/5/2023 15:41:38",
+		"Your name": "Sumit Sharma",
+		"Display your name under the message?": "No",
+		"Your note":
+			"Thank you sir for taking me under your guidance and providing all the encouragement and support in my journey so far in the spire lab.",
+	},
+	{
+		Timestamp: "9/5/2023 15:41:50",
+		"Your name": "Sandhya A",
+		"Display your name under the message?": "Yes",
+		"Your note":
+			"Your patience, kindness, and understanding have helped me to grow and succeed in ways that I never though possible. You are a mentor and a role model! I am so grateful for everythinf you have done for me. A Very Happiest Teacher's day to you sir.",
 	},
 ];
